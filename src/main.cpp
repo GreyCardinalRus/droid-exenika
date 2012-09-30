2f980da2dbda786967d77f5127ca4df3fe197e34
grey.c.rus <grey.c.rus@localhost>
2012-09-30 17:32:11 +0400

    ???????? ????? ???????? ????? -???? ?????

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,108 @@ using namespace std;
 
 #define PI 3.1415926535898
 
+class MarkerFinder {
+private:
+	// original image
+	cv::Mat img;
+	// vector containing the end points
+	// of the detected lines
+	std::vector<cv::Vec2i> dots;
+public:
+	MarkerFinder() {
+	}
+	;
+	std::vector<cv::Vec2i> findDots(cv::Mat& image) {
+		dots.clear();
+		int minX, minY;
+		brightFinder(image, &minX, &minY);
+		if ((minX == 0) && (minY == 0)) {
+			;
+		}
+		else
+		{
+		cv::Vec2i np ;//= new cv::Vec2i();
+		np[0]=minX;np[1]=minY;
+		dots.push_back(np);
+		//brightFinder(image, minX, minY);
+		//dots.insert(100,100);
+//				cv::HoughLinesP(binary, lines, deltaRho, deltaTheta, minVote, minLength,
+//						maxGap);
+		}
+		return dots;
+	}
+	// Draw the detected lines on an image
+	void drawDetectedDots(cv::Mat &image,cv::Mat &target,
+			cv::Scalar color = cv::Scalar(128, 128, 255),int size=20) {
+		// Draw the lines
+		std::vector<cv::Vec2i>::const_iterator it2 = dots.begin();
+		int minX, minY;
+		while (it2 != dots.end()) {
+		 minX =(*it2)[0]; minY = (*it2)[1];
+		//brightFinder(image, &minX, &minY);
+		if ((minX == 0) && (minY == 0)) {
+			return;
+		}
+		//cout << "found";
+		cv::Point ptc(minX, minY);
+		cv::circle(target, ptc, size, color, 1, 8);
+		cv::Point pt1(minX - size, minY );
+		cv::Point pt2(minX + size, minY );
+		cv::line(target, pt1, pt2, color);
+		cv::Point pt3(minX , minY - size);
+		cv::Point pt4(minX , minY+ size );
+		cv::line(target, pt3, pt4, color);
+		++it2;
+		}
+	}
+	//находим белое пятно
+	void brightFinder(cv::Mat &image, int* x, int* y) const {
+//		int height = image.rows;
+//		int width = image.cols;
+//		int step = 1;        //image->widthStep;
+//		int channels = image.channels();
+//		uchar *data = image.data;
+
+		//Ищем самое яркое пятно
+		int min = 255;
+		int minX = 0;
+		int minY = 0;
+
+		int nl = image.rows; // number of lines
+		// total number of elements per line
+		int nc = image.cols * image.channels();
+		for (int j = 0; j < nl; j++) {
+			// get the address of row j
+			uchar* data = image.ptr<uchar>(j);
+			for (int i = 0; i < nc; i++) {
+				// process each pixel ---------------------
+				if (data[i ] <= min) {
+					min = data[i];
+					minX = i;
+					minY = j;
+				}
+//				data[i] = data[i] / div * div + div / 2;
+				// end of pixel processing ----------------
+			}
+		}
+
+//		for (int i = 0; i < height; i++) {
+//			for (int j = 0; j < width; j++) {
+//				for (int k = 0; k < channels; k++) {
+//					if (data[i * step + j * channels + k] <= min) {
+//						min = data[i * step + j * channels + k];
+//						minX = j;
+//						minY = i;
+//					}
+//				}
+//			}
+//		}
+		if (nl<(minY+50)||nl<(minX+50)) {minX=0;minY=0;}
+		*x = minX;
+		*y = minY;
+	}
+};
+
 class LineFinder {
 private:
 // original image
@@ -98,16 +200,16 @@ int main(int argc, char** argv) {
 //	cv::imshow("Detected Lines with HoughP", image);
 //	waitKey(0);
 //
-	   // Open the default camera
-	    cv::VideoCapture capture(0);
-	    // Check if we succeeded
-	    if(!capture.isOpened())
-	    {
-	        std::cout<<"Video capture failed, please check the camera."<<std::endl;
-	        return -1;
-	    }else{
-	        std::cout<<"Video camera capture successful!"<<std::endl;
-	    }
+	// Open the default camera
+	cv::VideoCapture capture(0);
+	// Check if we succeeded
+	if (!capture.isOpened()) {
+		std::cout << "Video capture failed, please check the camera."
+				<< std::endl;
+		return -1;
+	} else {
+		std::cout << "Video camera capture successful!" << std::endl;
+	}
 //	    int Rmin = 0;
 //	    int Rmax = 256;
 //
@@ -118,55 +220,61 @@ int main(int argc, char** argv) {
 //	    int Bmax = 256;
 //
 //	    int RGBmax = 256;
-int Rmin=5,Rmax=255;
-	    for(;;) {
-	        cv::Mat frame,hsv,frameBitmap;
-	        cv::Mat grayFrame;
-	        cv::Mat gaussGrayFrame;
-	        cv::Mat edges;
-	        // create vector of 3 images
-	        std::vector<cv::Mat> planes;
+	int Rmin = 5, Rmax = 255;
+	for (;;) {
+		cv::Mat frame, hsv, frameBitmap;
+		cv::Mat grayFrame;
+		cv::Mat gaussGrayFrame;
+		cv::Mat edges;
+		// create vector of 3 images
+		std::vector<cv::Mat> planes;
 
-	        LineFinder finder;
+		LineFinder finder;
+		MarkerFinder mFinder;
 
-	        capture >> frame; // get a new frame from camera
-	        // split 1 3-channel image into 3 1-channel images
-	        cv::split(frame,planes);
-	        //planes[2]^=planes[2];
-	        cv::cvtColor(frame,hsv, CV_BGR2HSV );
-	        inRange(planes[2],Scalar(0,0,Rmin),Scalar(250,250,Rmax),frameBitmap);
-	        //frameBitmap = planes[2];                                                        // Переводим в bitmap
-	      // medianBlur(frameBitmap,frameBitmap,5); // фильтруем шумы
-	        //cv::split(hsv,planes);
+		capture >> frame; // get a new frame from camera
+		// split 1 3-channel image into 3 1-channel images
+		cv::split(frame, planes);
+		//planes[2]^=planes[2];
+		cv::cvtColor(frame, hsv, CV_BGR2HSV);
+		inRange(planes[2], Scalar(0, 0, Rmin), Scalar(250, 250, Rmax),
+				frameBitmap);
+		//frameBitmap.inv();
+		//frameBitmap = planes[2];                                                        // Переводим в bitmap
+//	        medianBlur(frameBitmap,frameBitmap,5); // фильтруем шумы
+		//cv::split(hsv,planes);
 //	        cv::inRange(planes[2],cvScalar(Rmin),cvScalar(Rmax),planes[2]);
-	        //Convert the frame into a gray Frame
-	        cv::cvtColor(frame, grayFrame, CV_BGR2GRAY);
+		//Convert the frame into a gray Frame
+		cv::cvtColor(frame, grayFrame, CV_BGR2GRAY);
 
-	        //Apply a Gaussian Blur on the gray-level Frame
-	        cv::GaussianBlur(grayFrame, gaussGrayFrame, cv::Size(7,7), 1.5, 1.5);
+		//Apply a Gaussian Blur on the gray-level Frame
+		cv::GaussianBlur(grayFrame, gaussGrayFrame, cv::Size(7, 7), 1.5, 1.5);
 
-	        //Apply Canny Algorithm
-	        cv::Canny(
-	            gaussGrayFrame, // gray-level source image
-	            edges,          // output contours
-	            0,              // low threshold
-	            30,             // high threshold
-	            3);             // aperture size
-	        //End Canny Algorithm
+		//Apply Canny Algorithm
+		cv::Canny(gaussGrayFrame, // gray-level source image
+				edges,          // output contours
+				0,              // low threshold
+				30,             // high threshold
+				3);             // aperture size
+		//End Canny Algorithm
 
-	        	finder.setLineLengthAndGap(100, 3);
-	        	finder.setMinVote(80);
-	        //Detect lines
-	        std::vector<cv::Vec4i> lines = finder.findLines(edges);
-	        //Draw the detected lines
-	        finder.drawDetectedLines(frame);
+		finder.setLineLengthAndGap(100, 3);
+		finder.setMinVote(80);
+		//Detect lines
+		std::vector<cv::Vec4i> lines = finder.findLines(edges);
+		//Draw the detected lines
+		mFinder.findDots( frameBitmap);
+		finder.drawDetectedLines(frame);
 
-	        //int threshold=250;
-	        //cv::threshold(frameBitmap, frameBitmap,threshold, 255, cv::THRESH_BINARY);
-	        cv::imshow("Camera Preview", frameBitmap);
+		//int threshold=250;
+		//cv::threshold(frameBitmap, frameBitmap,threshold, 255, cv::THRESH_BINARY);
+		mFinder.drawDetectedDots(frameBitmap,frame);
+		cv::imshow("Red Dots", frameBitmap);
+		cv::imshow("Camera Preview", frame);
 
-	        if(cv::waitKey(30) >= 0) break;
-	    }
+		if (cv::waitKey(30) >= 0)
+			break;
+	}
 
 	return 0;
 }
