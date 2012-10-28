cfea3181060c2d3ed927d6dd1b388b1dc2cb87b0
grey.c.rus <grey.c.rus@localhost>
2012-10-28 11:40:46 +0400

    hhh

--- src/app.cpp
+++ src/app.cpp
@@ -16,22 +16,68 @@
 
 #include "app.h"
 
+static int screen_width = 0;
+static int screen_height = 0;
 static int gAppAlive = 0;
 
 void appInit()
 {
-	//Run Stream process
-	at_run();
-	navdata_run();
-	//stream_run();
+	//int status;
 
-	gAppAlive = 1;
+   //Run AT command process
+   at_run();
+
+   //Run Navdata process
+   navdata_run();
+
+   //Run Stream process
+   stream_run();
+
+#ifdef BUILD_OGLES
+	// video rendering
+	video_init();
+#endif
+   gAppAlive = 1;
 }
 
 void appDeinit()
 {
 	INFO("shutting down application...\n");
 
-	gAppAlive = 0;
+   gAppAlive = 0;
+
+#ifdef BUILD_OGLES
+	video_deinit();
+#endif
+	// all threads should implement a loop polling gAppAlive
+	navdata_stop();
+	stream_stop();
+   at_stop();
+
 	INFO("application was cleanly exited\n");
+}
+
+#ifdef BUILD_OGLES
+void appRender(long tick, int width, int height)
+{
+	if (!gAppAlive) {
+		return;
+	}
+   screen_width = width;
+   screen_height = height;
+	video_render(tick, screen_width, screen_height);
+}
+#endif
+
+void get_screen_dimensions(int *w, int *h)
+{
+	if (screen_width) {
+		*w = screen_width;
+		*h = screen_height;
+	}
+	else {
+		// dummy dimensions
+		*w = 480;
+		*h = 320;
+	}
 }
